730b7985b6078d0ed18712f859ddcff1048231fa
Matthew Roberts <mrober13@uoguelph.ca>
2013-03-13 16:15:39 -0400

    adding gui

---
+++ formparse.php
@@ -0,0 +1,16 @@
+<?php {
+
+    $formblding = (isset($_GET['building']) ? $_GET['building'] : "");
+
+    $formday = (isset($_GET['day']) ? $_GET['day'] : "");
+
+    $formhour = (isset($_GET['hour']) ? $_GET['hour'] : "0");
+    $formmin = (isset($_GET['min']) ? $_GET['min'] : "0");
+    $formtime = "$formhour:$formmin";
+
+    $formcapacity = (isset($_GET['capacity']) ? $_GET['capacity'] : 0);
+
+    
+
+
+} ?>
